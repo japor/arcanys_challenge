f37c0a9b6dddd9984b43f7c0cc57a92f928eb453
japor <jonathan.mejoy@codeandtheory.com>
2023-06-04 17:41:19 +0800

    Task 3 : Create REST resource , listing all events

---
+++ web/modules/custom/arcanys_rest/src/Plugin/rest/resource/EventRestResource.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Drupal\arcanys_rest\Plugin\rest\resource;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Psr\Log\LoggerInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Provides a resource to get view modes by entity and bundle.
+ *
+ * @RestResource(
+ *   id = "event_rest_resource",
+ *   label = @Translation("Custom Get Rest Resource"),
+ *   uri_paths = {
+ *     "canonical" = "/api/event-listing"
+ *   }
+ * )
+ */
+class EventRestResource extends ResourceBase {
+  /**
+   * A current user instance which is logged in the session.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $loggedUser;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A currently logged user instance.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user,
+    EntityTypeManagerInterface $entity_type_manager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+
+    $this->loggedUser = $current_user;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('sample_rest_resource'),
+      $container->get('current_user'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * Responds to GET request.
+   * Returns a list of node events.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   *   Throws exception expected.
+   */
+  public function get() {
+    // Implementing our custom REST Resource here.
+    // Use currently logged user after passing authentication and validating the access of term list.
+    if (!$this->loggedUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+
+    $events = [];
+    $nodes = $this->entityTypeManager->getStorage('node')->loadByProperties(['type' => 'event']);
+
+    foreach ($nodes as $node) {
+      $image_field = $node->get('field_event_image')->entity;
+
+      $events[] = [
+        'id' => $node->id(),
+        'title' => $node->getTitle(),
+        'description' => $node->get('field_description')->value,
+        'start_time' => $node->get('field__start_time')->value,
+        'end_time' => $node->get('field_end_time')->value,
+        'event_image' => $image_field ? file_create_url($image_field->getFileUri()) : '',
+      ];
+    }
+
+    $response = new ResourceResponse($events);
+    $response->addCacheableDependency($events);
+    return $response;
+  }
+}
